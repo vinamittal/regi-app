d22bacaa5ad591809316daa9ebfc55a300f044e9
vinamittal <130295482+vinamittal@users.noreply.github.com>
2024-01-15 13:43:30 +0530

    Update backend.tf
    s3

--- Eks-Terraform/backend.tf
+++ Eks-Terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ratmdatastore" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
+    bucket = "regi-app"
+    key = "Jenkins/terraform.tfstate"
     region = "us-east-1"
   }
 }
